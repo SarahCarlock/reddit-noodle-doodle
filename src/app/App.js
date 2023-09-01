2e8e9363bb69a3802b34251fe41f9784d74a307f
Sarah Carlock <sarah.carlock86@gmail.com>
2023-09-01 10:10:54 -0500

    Move app items to app folder

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-import { Header } from './components/header/Header';
-import { Footer } from './components/footer/Footer';
+import { Header } from '../components/header/Header';
+import { Footer } from '../components/footer/Footer';
 
 
 export default function App() {
   return (
     <App className="App">
       <Header />
-
+        <p>Howdy</p>
       <Footer />
     </App>
   );
